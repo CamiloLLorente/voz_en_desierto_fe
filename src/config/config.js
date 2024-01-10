f864f0646804252c1fba5535385c907c867bd59a
CamiloLLorente <camiloandresllorente@gmail.com>
2024-01-10 11:48:36 -0500

    Agregando ruta en la configuración

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,5 @@
 export const COLOR_STAR_SELECTED = "#E8D959";
 export const COLOR_STAR_NOT_SELECTED = "#AAAAAA";
+export const URL_SIFINCA_DEMO = 'https://www.sifinca.net/demos/web/app_dev.php';
+export const URL_SIFINCA_CTG = 'https://www.sifinca.net/sifinca/web/app.php';
+export const URL_SIFINCA_MTR = 'https://www.sifinca.net/sifincam/web/app.php';
