9475b70c99ccad27ecc4a29295a42b21436598d6
CamiloLLorente <camiloandresllorente@gmail.com>
2024-01-16 19:26:06 -0500

    Cambiando BrowserRouter por HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter ,  Routes, Route } from 'react-router-dom';
+import { BrowserRouter , HashRouter, Routes, Route } from 'react-router-dom';
 import { GlobalProvider } from "./context/GlobalState";
 import Home from './pages/general/Home';
 import Survey from './pages/survey/Survey';
@@ -12,17 +12,18 @@ function App() {
   return (
     <GlobalProvider>
       <Header />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
 
         <Route path="/" element={<Home />} />
+        <Route path="/header" element={<Header />} />
         {/* <Route path="/survey" element={<Survey />} /> */}
         <Route path="/survey/:city/:id" element={<Survey />}  />
         
       
       
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </GlobalProvider>
   )
 }
