e0f0012fa6b3842947875b2fec08f3237f941021
CamiloLLorente <camiloandresllorente@gmail.com>
2024-01-16 19:26:42 -0500

    Modifincando forma de importar logo

--- src/components/general/header/Header.jsx
+++ src/components/general/header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './header.module.scss'
+import logo_ays_header from "../../../assets/logo_ays_header.png";
 
 function Header() {
     return (
         <header className={style.header}>
-            <img src="./src/assets/logo_ays_header.png"></img>
+            <img src={logo_ays_header}></img>
         </header>
     );
 }
